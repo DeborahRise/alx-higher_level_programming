e50464031985a491bfe78df6340821da63adbc60
DeborahRise <debie.rise.bookings>
2023-11-02 18:25:58 +0000

    infinite

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    ag = sys.argv[1:]
+    num = len(ag)
+    result = 0
+    print(num)
+    for r in range(num):
+        result = result + int(ag[r])
+    print("{}".format(result))
---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    result = dir(hidden_4)
+    for names in result:
+        if result[0:2] != "__":
+            print(result)
