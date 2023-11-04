7ea127749c5404354e5d912149a9ffc27975d0ba
DeborahRise <debie.rise.bookings>
2023-11-04 17:58:26 +0000

    formatted

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for r in matrix:
         for _r in range(len(r) - 1):
-            print("{}".format(r[_r]), end=' ')
+            print("{:d}".format(r[_r]), end=' ')
         if len(r) > 0:
-            print("{}".format(r[-1]))
+            print("{:d}".format(r[-1]))
         else:
             print()
