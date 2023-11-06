0cb650105c5927d47e8d5bb1d6ead99f50b75762
DeborahRise <debie.rise.bookings>
2023-11-06 21:27:09 +0000

    palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,53 +3,49 @@
 /**
  * is_palindrome - a function that checks for a palindrome
  * in a singly linked list
- * @head: head of the node
- * Return: 0 if not, 1 if it is
+ * @head: a double pointer to the head of the list
+ * Return: 0 if not a palindrome, 1 if it is
  */
 
 int is_palindrome(listint_t **head)
 {
-	listint_t *turtle = *head;
-	listint_t *hare = *head;
-	listint_t *temp, *first_half, *second_half;
-
-	if (!(*head) || !head || !((*head)->next))
+	if (*head == NULL)
 		return (1);
 
+	listint_t *turtle = *head;
+	listint_t *hare = *head;
+
+	/* Find the middle of the linked list */
 	while (hare && hare->next)
 	{
 		turtle = turtle->next;
 		hare = hare->next->next;
 	}
 
+	/* Reverse the second half of the linked list */
+	listint_t *prev = NULL;
+	listint_t *next = NULL;
+	listint_t *current = turtle;
 
-	hare = turtle->next;
-	temp = NULL;
-	turtle->next = NULL;
-
-	while (hare && hare->next)
+	while (current)
 	{
-		temp = hare;
-		hare = hare->next;
-		temp->next = second_half;
-		second_half = temp;
+		next = current->next;
+		current->next = prev;
+		prev = current;
+		current = next;
 	}
-	first_half = *head;
 
+	listint_t *second_half = prev;
+	listint_t *first_half = *head;
+
+	/* Compare the first half and reversed second half */
 	while (second_half)
 	{
 		if (second_half->n != first_half->n)
 			return (0);
-		if (second_half->next && first_half->next)
-		{
-			second_half = second_half->next;
-			first_half = first_half->next;
-		}
-		else
-		{
-			second_half = NULL;
-			first_half = NULL;
-		}
+		second_half = second_half->next;
+		first_half = first_half->next;
 	}
+
 	return (1);
 }
