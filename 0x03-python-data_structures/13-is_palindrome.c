ab6e0f38d1f836aa5ccc627d77d874220979ad68
DeborahRise <debie.rise.bookings>
2023-11-06 20:15:19 +0000

    palindrome

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - a function that checks for a palindrome
+ * in a singly linked list
+ * @head: head of the node
+ * Return: 0 if not, 1 if it is
+ */
+
+int is_palindrome(listint_t **head)
+{
+	listint_t *turtle = *head;
+	listint_t *hare = *head;
+	listint_t *temp, *first_half, *second_half;
+
+	if (!(*head) || !((*head)->next))
+		return (1);
+
+	while (hare && hare->next)
+	{
+		turtle = turtle->next;
+		hare = hare->next->next;
+	}
+
+
+	hare = turtle->next;
+	temp = NULL;
+	turtle->next = NULL;
+
+	while (hare && hare->next)
+	{
+		temp = hare;
+		hare = hare->next;
+		temp->next = second_half;
+		second_half = temp;
+	}
+	first_half = *head;
+
+	while (second_half)
+	{
+		if (second_half->n != first_half->n)
+			return (0);
+		second_half = second_half->next;
+		first_half = first_half->next;
+	}
+	return (1);
+}
