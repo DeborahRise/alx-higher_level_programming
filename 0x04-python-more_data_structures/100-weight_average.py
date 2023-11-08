f6154a2558b1ffc4b74dfc6c100363cc45151bbb
DeborahRise <debie.rise.bookings>
2023-11-08 23:52:13 +0000

    weighted

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    if len(my_list) == 0:
+        return 0
+
+    score = weight = 0
+    for tup in my_list:
+        score += (tup[0] * tup[1])
+        weight += (t[1])
+    return (score / weight)
