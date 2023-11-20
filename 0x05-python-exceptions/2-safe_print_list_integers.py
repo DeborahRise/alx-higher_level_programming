8c91af05dd6eee8b616f8b58502cccedeb879240
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-20 21:51:43 +0000

    oh, i wasnt supposed to handle error

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,8 +5,6 @@ def safe_print_list_integers(my_list=[], x=0):
         try:
             print("{:d}".format(my_list[i]), end="")
             value += 1
-        except IndexError as err:
-            print("{}".format(err))
         except (TypeError, ValueError):
             continue
     print()
