d9ad358de2c33d51d9c9dc2b4a3f91feb47d0d1b
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-21 22:27:46 +0000

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-square.py - Deborah Rise
+"""Creating an empty square class"""
+
+
+class Square:
+    """Type: an empty class Square that defines a square"""
+    def __init__(self, size):
+        self.__size = size
