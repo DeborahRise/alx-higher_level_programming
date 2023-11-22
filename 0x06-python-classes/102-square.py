b3b4bb32e6332edd0f7dc535ccc2b6f1210efc9d
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-22 04:34:46 +0000

    compare 2 squares

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# 4-square.py - Deborah Rise
+"""Updating a square by: (based on 3-square.py)"""
+
+
+class Square:
+    """Type: an empty class Square that defines a square"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = (size)
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+
+    def __eq__(self, other):
+        return self.area() == other.area()
+
+    def __ne__(self, other):
+        return self.area() != other.area()
+
+    def __lt__(self, other):
+        return self.area() < other.area()
+
+    def __le__(self, other):
+        return self.area() <= other.area()
+
+    def __gt__(self, other):
+        return self.area() > other.area()
+
+    def __ge__(self, other):
+        return self.area() >= other.area()
