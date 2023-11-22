37fa889d2a4bc20c415507f757cea4fe227b34e3
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-22 02:38:10 +0000

    please

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -43,10 +43,10 @@ class Square:
 
     def my_print(self):
         if self.__size == 0:
-            print()
+            print("")
         else:
-            for i in range(0, self.position[1]):
+            for i in range(self.__position[1]):
                 print()
-            for i in range(0, self.__size):
-                print(" " * self.position[0], end="")
+            for i in range(self.__size):
+                print(" " * self.__position[0], end="")
                 print("#" * self.__size)
