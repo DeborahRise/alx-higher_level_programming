8acbb8df2b8c1af2ab0bd61a94f056f41cba7fe5
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-22 01:40:01 +0000

    cordinates of square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,7 +44,9 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        for i in range(self.__size):
-            for j in range(self.__size):
-                print("#", end='')
-            print()
+        else:
+            for i in range(0, self.position[1]):
+                print()
+            for i in range(0, self.__size):
+                print(" " * self.position[0], end="")
+                print("#" * self.__size)
