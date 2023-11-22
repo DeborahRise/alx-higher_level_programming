fd741a519c06afa9ae8c5c961bdbbf999322bfc4
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-22 15:34:35 +0000

    task 6 again

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -6,12 +6,8 @@
 class Square:
     """Type: an empty class Square that defines a square"""
     def __init__(self, size=0, position=(0, 0)):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = (size)
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         return (self.__size * self.__size)
@@ -47,6 +43,6 @@ class Square:
         else:
             for i in range(0, self.position[1]):
                 print()
-            for i in range(0, self.__size):
+            for i in range(0, self.size):
                 print(" " * self.position[0], end="")
-                print("#" * self.__size)
+                print("#" * self.size)
