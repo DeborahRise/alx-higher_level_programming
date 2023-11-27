c1105999273cc0b7d512e84c4ce44162251a9916
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-27 13:36:34 +0000

    an empty rectangle class

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 # 0-rectangle.py - Deborah Rise
-"""An empty class Rectangle"""
+"""
+An empty class Rectangle
+
+"""
 
 
 class Rectangle:
-    """Type: This class, defines a Rectangle"""
+    """ Type: This class, defines a Rectangle"""
     pass
