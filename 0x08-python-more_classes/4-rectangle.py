b64f8639ae9cb3fe9a3047ae55ef80990f4e44e9
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-27 16:34:51 +0000

    using repr

---
+++ 0x08-python-more_classes/4-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+"""
+An class Rectangle
+
+"""
+
+
+class Rectangle:
+    """ Type: This class, defines a Rectangle"""
+
+    def __init__(self, width=0, height=0):
+        """ instantiation of class """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__width * self.__height
+
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.__height == 0 or self.__width == 0:
+            return ("")
+        to_str = ""
+        for i in range(self.__height):
+            for j in range(self.__width):
+                to_str += '#'
+            if i != self.__height - 1:
+                to_str += '\n'
+        return (to_str)
+
+    def __repr__(self):
+        return f"Rectangle({self.__height}, {self.__width})"
