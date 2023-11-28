3e7a7e77290ce08936aac62d59eeefac3c62f7c9
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-28 22:28:22 +0000

    task locked class and task 34

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""A module for a locked class"""
+
+
 class LockedClass:
+    """
+    Will only allow the instation of an attribute
+    called: first_name
+    """
+
     __slots__ = ["first_name"]
