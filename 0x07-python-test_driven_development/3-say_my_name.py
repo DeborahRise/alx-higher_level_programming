abd46af10001f3d1683fcf9c04cbca10a9297481
DeborahRise <debie.rise.bookings@gmail.com>
2023-11-29 05:09:18 +0000

    trailing white space task3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -12,12 +12,12 @@ def say_my_name(first_name, last_name=""):
     Arg1: type str first name
     Arg2: type str last name
     Raise: TypeError
-    
+
     """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
 
     if not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
-   
+
     print("My name is {} {}".format(first_name, last_name))
