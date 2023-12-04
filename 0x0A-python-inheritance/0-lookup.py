d653e095d505e2b145fe7901b6e270859c2324a9
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-04 22:38:43 +0000

    look_up

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" A module to lookup available attributes"""
+
+
 def lookup(obj):
     """ returns the list of available attr/Meths"""
-    return (dir(obj))
+
+    return dir(obj)
