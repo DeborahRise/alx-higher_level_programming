6043bfb28fa2c98dc3a02e8474d2b87137992408
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-04 23:36:52 +0000

    instance of a subclass, not the superclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""  a function that returns True
+if the object is an instance of a class that inherited
+(directly or indirectly) from the specified class;
+otherwise False
+"""
+
+def inherits_from(obj, a_class):
+    """ The function
+    Args:
+    obj: object in question
+    a_class: class checked
+    """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    return False
