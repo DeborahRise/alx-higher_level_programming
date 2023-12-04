dc787283f13c5f95778ebae02b95141b6cb4569d
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-04 20:06:52 +0000

    list of available attribs. using dir(obj)

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-
 def lookup(obj):
-    """
-    a function that returns the list of available
-    attributes and methods of an object:
-    return [getattr(obj, name) for name in dir(obj)]
-    """
-    return dir(obj)
+    """ returns the list of available attr/Meths"""
+    return (dir(obj))
