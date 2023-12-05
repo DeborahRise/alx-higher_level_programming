306a658252388926911d00081123f60a91a49343
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-05 01:35:05 +0000

    rectangle

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" BaseGeometry module
+"""
+
+
+class BaseGeometry:
+    """ BaseGeometry class
+    """
+
+    def area(self):
+        """ A public instance method
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ Another public instance method
+        """
+        self.name = name
+        self.value = value
+
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ The class Rectangle
+    """
+
+    def __init__(self, width, height):
+        """ Initialization
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
