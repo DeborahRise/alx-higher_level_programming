a146e51e934258a83a4eefb2145fac435f139c33
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-05 22:53:06 +0000

    load from json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Crates an Object 
+from a JSON file
+"""
+
+
+import json
+"""Calling the json module
+"""
+
+
+def load_from_json_file(filename):
+    """ serialization
+    encoding of a string
+    """
+
+    with open(filename, "r", encoding='utf-8') as f:
+        json.load(f)
