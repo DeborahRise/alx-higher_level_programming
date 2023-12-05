e26ffec7fc321bdc946696023d5d41e6e867773c
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-05 01:41:30 +0000

    name and value attr removed

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -1,25 +1,7 @@
 #!/usr/bin/python3
 """ BaseGeometry module
 """
-
-
-class BaseGeometry:
-    """ BaseGeometry class
-    """
-
-    def area(self):
-        """ A public instance method
-        """
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """ Another public instance method
-        """
-
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+BaseGeometry = __import__("7-base_geometry.py").BaseGeometry
 
 
 class Rectangle(BaseGeometry):
