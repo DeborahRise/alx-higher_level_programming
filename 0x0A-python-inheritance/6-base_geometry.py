03d1f8ac9a315db7a8f020c3f61ec5e30f6e7fdb
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-05 00:00:31 +0000

    Raise Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" BaseGeometry module
+"""
+
+
+class BaseGeometry:
+    """ BaseGeometry class
+    """
+
+    def area(self):
+        """ A public instance method
+        """
+        raise Exception("area() is not implemented")
