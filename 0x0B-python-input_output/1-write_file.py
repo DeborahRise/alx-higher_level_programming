bcbe3bb84822d0fc9f418379fdca99aff1872f32
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-05 21:37:07 +0000

    write to text file, return num of char

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" writing a string to a text file
+"""
+
+def write_file(filename="", text=""):
+    """ Must use with statement
+    no file permission
+    """
+
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
+        return (len(text))
