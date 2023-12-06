801b3716ec3c73f00ed31fb8d25b106ab1bdc26c
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-06 01:13:07 +0000

    class to JAY

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""This module defines a function that returns the dictionary description with simple data structure (list, dictionary,"""
+
+
+def class_to_json(obj):
+    """Returns the dictionary representation of a simple data structure"""
+    return obj.__dict__
