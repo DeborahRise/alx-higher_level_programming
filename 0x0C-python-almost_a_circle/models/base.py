1d3ddabfa430e31442653e31ca619d5cf6ea6681
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-12 01:34:43 +0000

    15 and 16

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,15 +27,18 @@ class Base:
     def to_json_string(list_dictionaries):
         """ returns the json rep of list_dictionaries
         """
-        if list_dictionaries is None or len(list_dictionaries) == 0:
-            return []
+        if list_dictionaries is None or len(list_dictionaries) == []:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        if list_objs is None:
-            list_objs = []
-        json_string = cls.to_json_string(
-                [obj.to_dictionary() for obj in list_objs])
-        with open(f"{cls.__name__}.json", "w") as file:
-            file.write(json_string)
+        """ writes json string rep to file
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as f:
+            if list_objs is None:
+                f.write("[]")
+            else:
+                list_dictionaries = [o.to_dictionary() for o in list_objs]
+                f.write(Base.to_json_string(list_dictionaries))
