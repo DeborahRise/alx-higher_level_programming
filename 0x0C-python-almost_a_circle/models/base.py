56b008c445d4a8161640576b9d40da6722abbad4
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-12 01:56:48 +0000

    17 and 18

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,3 +42,24 @@ class Base:
             else:
                 list_dictionaries = [o.to_dictionary() for o in list_objs]
                 f.write(Base.to_json_string(list_dictionaries))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ returns a list of json string rep"""
+
+        if json_string is None or json_string == '[]':
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes
+        already set
+        """
+
+        if cls.__name__ == "Rectangle":
+            new_instance = cls(5, 5)
+        else:
+            new_instance = cls(5)
+        new_instance.update(**dictionary)
+        return new_instance
