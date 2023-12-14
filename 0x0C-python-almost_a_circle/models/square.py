808da28567947a3c49806d5f331ad6e9e7d485a4
DeborahRise <debie.rise.bookings@gmail.com>
2023-12-14 16:57:39 +0000

    more tests for the square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,73 +1,68 @@
 #!/usr/bin/python3
-""" And Now The Square
-"""
-
+"""This module contains a square class"""
 
 from models.rectangle import Rectangle
-""" activating/importing the Rectangle class
-"""
 
 
 class Square(Rectangle):
-    """ The square inherits every attribute from the
-    Rectangle
-    """
-
+    """Represents a square"""
     def __init__(self, size, x=0, y=0, id=None):
-        """ square class instantiation method """
-
-        if not isinstance(size, int):
-            raise TypeError("width must be an integer")
-        if size <= 0:
-            raise ValueError("width must be > 0")
-
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = None
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        """ returns a string of attribute"""
-
-        return (
-                "[Square] ({}) {}/{} - {}".format(
-                    self.id, self.x, self.y, self.width)
-                )
+        """Defines a format for the string representation of the class"""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
 
     @property
     def size(self):
-        """ adding the public getter """
-
-        return self.width
+        """Gets the value of size"""
+        return self.__width
 
     @size.setter
     def size(self, value):
-        """ adding public setter """
-
-        if not isinstance(value, int):
+        """Sets the value for size"""
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-        self.width = value
-        self.height = value
+        self.__width = value
+        self.__height = value
 
     def update(self, *args, **kwargs):
-        """ updating the class square
-        by adding public method
-        """
+        """Updates attributes of an instance"""
 
-        if args:
+        if args is not None and len(args) != 0:
             if len(args) >= 1:
+                if type(args[0]) != int and args[0] is not None:
+                    raise TypeError("id must be an integer")
                 self.id = args[0]
-            if len(args) >= 2:
+            if len(args) > 1:
                 self.size = args[1]
-            if len(args) >= 3:
+            if len(args) > 2:
                 self.x = args[2]
-            if len(args) >= 4:
+            if len(args) > 3:
                 self.y = args[3]
-        elif kwargs:
+        else:
             for key, value in kwargs.items():
-                setattr(self, key, value)
+                if key == "id":
+                    if type(value) != int and value is not None:
+                        raise TypeError("id must be an integer")
+                    self.id = value
+                if key == "size":
+                    self.size = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
 
     def to_dictionary(self):
-        """ returns the dictionary representation of a square
-        """
+        """Returns the dictionary representation of a Square"""
 
-        return {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
+        obj_dictionary = {'id': self.id, 'size': self.size, 'x': self.x,
+                          'y': self.y}
+
+        return obj_dictionary
