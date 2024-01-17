7747d7a4011bd9861d98551dd58b589c17d1ca6c
DeborahRise <debie.rise.bookings@gmail.com>
2024-01-17 01:48:07 +0000

    Script to create a non existent db

---
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,5 @@
+--  a script that creates the database hbtn_0c_0
+-- in MySQL server
+-- If the database hbtn_0c_0 already exists,
+-- your script should not fail
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
