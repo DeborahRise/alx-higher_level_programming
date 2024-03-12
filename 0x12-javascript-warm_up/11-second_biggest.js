4c014d5d4dea4c9bbd9b862dbe15428f5f1bd64c
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-12 09:18:47 +0000

    return 2nd largest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (args.length === 2 || args.length === 3) {
+  console.log(0);
+}
+let largeNum = 0;
+let secNum = 0;
+
+for (let i = 2; i < args.length; i++) {
+  if (args[i] > largeNum) {
+    secNum = largeNum;
+    largeNum = args[i];
+  }
+}
+console.log(secNum);
