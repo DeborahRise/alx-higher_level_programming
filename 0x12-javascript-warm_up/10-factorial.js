7a690272f39ae8f881984ed06294324803381d63
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-12 04:57:56 +0000

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+function factorial (a) {
+  if (isNaN(a)) {
+    return 1;
+  }
+  const numA = parseInt(a);
+  if (numA <= 1) {
+	  return 1;
+  }
+
+  return numA * factorial(numA - 1);
+}
+
+const a = process.argv[2];
+
+if (a === undefined || isNaN(a)) {
+  console.log('NaN');
+} else {
+  console.log(factorial(a));
+}
