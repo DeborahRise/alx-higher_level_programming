ad04ddd22e939850f19ee51baa9f214d8e98f9b4
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-12 18:58:07 +0000

    test

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    } else {
+      return {};
+
+      /*  if (Number(w) <= 0 || Number(h) <= 0) {
+    let Rectangle = new object(); */
+    }
+  }
+}
+
+module.exports = Rectangle;
