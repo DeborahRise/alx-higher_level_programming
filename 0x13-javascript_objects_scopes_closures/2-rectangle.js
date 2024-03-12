b349fa1cf5b05107a8e7f86b01e0d8e5ad81b482
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-12 21:16:33 +0000

    semi standard task 2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,10 @@
 
 class Rectangle {
   constructor (w, h) {
-	if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
       this.width = w;
       this.height = h;
-	} 
+    }
   }
 }
 
