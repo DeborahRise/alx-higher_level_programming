ea03515adf8ca26c9e11a1f490d6113cd02b0835
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-12 02:02:36 +0000

    using process.argv property

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (args.length > 3) {
+  console.log('Arguments found');
+} else if (args.length === 3) {
+  console.log('Argument found');
+} else {
+  console.log('No argument');
+}
