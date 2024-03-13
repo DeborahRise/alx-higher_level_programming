c011028c29b927607ac4f45b2beb82b9d053b639
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-13 04:56:21 +0000

    tests

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,19 +1,4 @@
-#!/usr/bin/node
-
+#!/usr/bin/Node
 exports.converter = function (base) {
-  return function convert(number) {
-    if (number === 0) {
-      return '';
-	} else {
-      const	remainder = number % base;
-      const quotient = Math.floor (number / base)
-
-      if (remainder < 10) {
-        digit = remainder;
-
-	  }
-    return number.tostring(base);
-  };
+  return num => num.toString(base);
 };
-
-exports.modules = { base };
