fa971b233900c68168d4b930379de742b1732c93
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-17 11:32:39 +0000

    added doc to task

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+script should take 3 arguments: mysql username, mysql password and database name
+script should connect to a MySQL server running on localhost at port 3306
+"""
+
+if __name__ == "__main__":
+
 import MySQLdb
 import sys
 
