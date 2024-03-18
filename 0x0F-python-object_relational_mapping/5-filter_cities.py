d4a4aea4dd1bce68a1c28dc85fb335f0a00ab192
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-18 02:46:27 +0000

    list cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+"""
+a script that lists all cities from the database hbtn_0e_0_usa
+script should take 3 arguments:
+mysql username, mysql password and database name
+script should connect to a MySQL server running on localhost at port 3306
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    db = MySQLdb.connect(
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
+
+    cur = db.cursor()
+    state_name = sys.argv[4]
+
+    query = """
+        SELECT cities.name
+        FROM cities
+        INNER JOIN states ON cities.state_id = states.id
+        WHERE states.name = %s
+        ORDER BY cities.id ASC
+        """
+    cur.execute(query, (state_name,))
+
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
