2a88eac20df358cb0fc98960a9a2ef69f1b5b92e
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-18 01:00:43 +0000

    safe filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -19,9 +19,8 @@ if __name__ == "__main__":
         )
     cur = db.cursor()
     state_name = sys.argv[4]
-
-    cur.execute("""SELECT * FROM states WHERE name = '{}'"""
-            .format(state_name))
+    query = """SELECT * FROM states WHERE name=%s ORDER BY id ASC;"""
+    cur.execute(query, (state_name,))
 
     rows = cur.fetchall()
 
