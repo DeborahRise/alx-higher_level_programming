12b6251f5645a98227be729e119cc692af56aea9
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-22 16:12:47 +0000

    corrected task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     state_name = sys.argv[4]
 
     query = """
-        SELECT *
+        SELECT cities.name
         FROM cities
         JOIN states ON cities.state_id = states.id
         WHERE states.name = %s
