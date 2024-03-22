45a22f756e130bfd5073de5e0bb55e543113b17e
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-22 16:24:29 +0000

    task 5

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -11,6 +11,8 @@ if __name__ == "__main__":
     import sys
     import MySQLdb
     db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
         db=sys.argv[3]
