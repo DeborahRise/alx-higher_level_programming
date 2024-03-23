1ddcc44ecbbdc15087e7432d9b88cabdc62a51db
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-23 00:29:44 +0000

    task 15

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
 
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
 
