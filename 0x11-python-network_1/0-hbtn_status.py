3243fb07fe9775f5629ee9d696916b1d6c344a31
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-28 17:12:09 +0000

    a Python script that fetches https://alx-intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import urllib.request
+
+url = 'https://alx-intranet.hbtn.io/status'
+
+with urllib.request.urlopen(url) as response:
+    html_content = response.read().decode('utf-8')
+
+
+print("Body response:")
+print("\t- type: {}".format(type(html_content)))
+print("\t- content: {}".format(html_content))
