8589f608d884cadece5de4ab3f51573cc82ba3cc
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-28 17:28:48 +0000

    edited the print statement

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,13 @@ https://alx-intranet.hbtn.io/status """
 import urllib.request
 
 if __name__ == "__main__":
+
     url = 'https://alx-intranet.hbtn.io/status'
 
     with urllib.request.urlopen(url) as response:
-        html_content = response.read().decode('utf-8')
+        html_content = response.read()
 
         print("Body response:")
         print("\t- type: {}".format(type(html_content)))
         print("\t- content: {}".format(html_content))
+        print("\t- utf8 content: {}".format(html_content.decode('utf-8')))
