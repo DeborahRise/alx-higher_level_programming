e7f106b16df1740951c508b3779e87a70a78ec3b
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-28 23:05:57 +0000

    POST and requests

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""  a Python script that fetches
-https://alx-intranet.hbtn.io/status """
-
+"""
+a Python script that fetches https://alx-intranet.hbtn.io/status
+"""
 import requests
 
 if __name__ == "__main__":
