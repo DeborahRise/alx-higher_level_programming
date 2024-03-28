78d94fb265672422656c46991138c4271fa26520
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-28 03:05:23 +0000

    2-delete.sh  to 6-peak.py

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# a Bash script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+# A header variable X-School-User-Id must be sent with the value 98
+# You have to use curl
+curl -sH "X-School-User-Id: 98" "$1"
---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+""" Technical interview preparation """
+
+
+def find_peak(list_of_integers):
+    """ 
+     a function that finds a peak in a list of unsorted integers.
+     """
+
+    loi = list_of_integers
+    if loi == []:
+        return None
+    total = len(loi)
+    if total == 1:
+        return loi[0]
+    elif total == 2:
+        return max(loi)
+    middle = int(total / 2)
+    peak = loi[middle]
+    if peak > loi[middle - 1] and peak > loi[middle + 1]:
+        return peak
+    elif peak < loi[middle - 1]:
+        return find_peak(loi[:middle])
+    else:
+        return find_peak(loi[middle + 1:])
