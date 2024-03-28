22a9a709635c4c1f427a77b06625a4b0a2f7a155
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-28 17:20:27 +0000

    a Python script that fetches https://alx-intranet.hbtn.io/status new

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,12 +4,12 @@ https://alx-intranet.hbtn.io/status """
 
 import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
 
-with urllib.request.urlopen(url) as response:
-    html_content = response.read().decode('utf-8')
+    with urllib.request.urlopen(url) as response:
+        html_content = response.read().decode('utf-8')
 
-
-print("Body response:")
-print("\t- type: {}".format(type(html_content)))
-print("\t- content: {}".format(html_content))
+        print("Body response:")
+        print("\t- type: {}".format(type(html_content)))
+        print("\t- content: {}".format(html_content))
