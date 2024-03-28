77473486672db372410000db2577710a171dde99
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-28 19:51:45 +0000

    displays the value of the X-Request-Id 2

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -10,10 +10,11 @@ in the header of the response
 import urllib.request
 import sys
 
-url = sys.argv[1]
+if __name__ == '__main__':
+    url = sys.argv[1]
 
-with urllib.request.urlopen(url) as response:
-    # Check if 'X-Request-Id is present in the responseheader
-    if 'X-Request-Id' in response.header:
-        request_id = response.header['X-Request-Id']
-        print(request_id)
+    with urllib.request.urlopen(url) as response:
+        # Check if 'X-Request-Id is present in the responseheader
+        if 'X-Request-Id' in response.headers:
+            request_id = response.headers['X-Request-Id']
+            print(request_id)
