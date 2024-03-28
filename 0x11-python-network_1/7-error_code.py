22198a7fc769a2d815272072b74607aeec05f457
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-28 23:39:16 +0000

    get and error handling

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+a Python script that takes in a URL,
+sends a request to the URL and displays the body
+of the response (decoded in utf-8).
+"""
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    response = requests.get(url)
+
+    # Display the body of the response
+    print(response.text)
+
+    # Check if HTTP status code is greater than or equal to 400
+    if response.status_code >= 400:
+        print(f'Error code: {response.status_code}')
