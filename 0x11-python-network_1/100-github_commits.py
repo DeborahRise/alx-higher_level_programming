b3e1fae9ca65b31bf2ab9f0849a04a9286f6d8d6
DeborahRise <debie.rise.bookings@gmail.com>
2024-03-29 03:33:44 +0000

    printing commits

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+a Python script that takes 2 arguments in order to solve this challenge.
+list 10 commits (from the most recent to oldest)
+of the repository “rails” by the user “rails”
+You must use the GitHub API, here is the documentation
+https://developer.github.com/v3/repos/commits/
+Print all commits by: `<sha>: <author name>` (one by line)
+The first argument will be the repository name
+The second argument will be the owner name
+"""
+
+import requests
+from requests.auth import HTTPBasicAuth
+import sys
+
+if __name__ == '__main__':
+    repo_name = sys.argv[1]
+    owner_name = sys.argv[2]
+
+    # Github endpoint for user info
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+            owner_name, repo_name)
+
+    response = requests.get(url)
+    commits = response.json()
+
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get('sha'),
+                commits[i].get('commit').get('author').get(''nameb)
+                ))
+    print(response.json().get('id'))
