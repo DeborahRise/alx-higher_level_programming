e2790c24ac0c844e9ea4f47d9101c926f63c2b98
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-24 01:26:28 +0100

    made 0-readme executable, changed shebang

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,4 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 /**
  * Write a script that reads and prints the content of a file.
  * The first argument is the file path
@@ -7,7 +7,22 @@
  */
 
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
-    if (err) throw err;
-    console.log(data); // File content
-  });
+const fs = require('fs');
+
+// Check if file path is provided as an argument
+if (process.argv.length < 3) {
+  console.error('Usage: ./0-readme.js <file_path>');
+  process.exit(1);
+}
+
+// Extract file path from command line arguments
+const filePath = process.argv[2];
+
+// Read file content
+fs.readFile(filePath, 'utf8', (err, data) => {
+  if (err) {
+    console.error(err); // Print error object if reading fails
+  } else {
+    console.log(data); // Print file content if reading succeeds
+  }
+});
