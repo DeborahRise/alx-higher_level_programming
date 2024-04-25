2e786ea4b1bf11c703118b16607603bdd50b6a70
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-25 05:04:01 +0100

    completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,42 +4,22 @@
  */
 
 const request = require('request');
+const url = process.argv[2];
 
-// Check if API URL is provided as an argument
-if (process.argv.length < 3) {
-  console.error('Usage: node completed_tasks.js <api_url>');
-  process.exit(1);
-}
-
-// Extract API URL from command line arguments
-const apiUrl = process.argv[2];
-
-// Send GET request to the JSONPlaceholder API to fetch todos
-request.get(apiUrl, (error, response, body) => {
+request(url, function (error, response, body) {
   if (error) {
-    console.error(error);
-  } else {
-    if (response.statusCode === 200) {
-      const todos = JSON.parse(body);
-      const completedTasksByUser = {};
-
-      // Iterate over todos to count completed tasks for each user
-      todos.forEach(todo => {
-        if (todo.completed) {
-          if (completedTasksByUser[todo.userId]) {
-            completedTasksByUser[todo.userId]++;
-          } else {
-            completedTasksByUser[todo.userId] = 1;
-          }
-        }
-      });
-
-      // Print users with completed tasks
-      console.log('{');
-      Object.keys(completedTasksByUser).forEach((userId, index, array) => {
-        console.log(`  '${userId}': ${completedTasksByUser[userId]}${index === array.length - 1 ? '' : ','}`);
-      });
-      console.log('}');
+    console.error('error:', error);
+  }
+  const tasks = JSON.parse(body);
+  const completed = {};
+  for (const task of tasks) {
+    if (task.completed) {
+      if (completed[task.userId] === undefined) {
+        completed[task.userId] = 1;
+      } else {
+        completed[task.userId]++;
+      }
     }
   }
+  console.log(completed);
 });
