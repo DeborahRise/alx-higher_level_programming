dc4f90bb80cb572a67f28811711e36bb27dae36d
DeborahRise <debie.rise.bookings@gmail.com>
2024-04-25 05:38:00 +0100

    101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,37 +1,47 @@
 #!/usr/bin/node
 /**
- * Write a script that   prints the number of movies where
+ * Write a script that prints all characters of a Star Wars movie:
+ * The first argument is the Movie ID - example: 3
+ * = “Return of the Jedi”
+ * Display one character name by line in the same order of the list
+ * “characters” in the /films/ response
+ * You must use the Star wars API
+ * You must use the module request
  */
 
 const request = require('request');
 
-const movieId = process.argv[2];
-const url = `https://swapi.dev/api/films/${movieId}/`;
-let characters = [];
-
-request(url, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-
-  const data = JSON.parse(body);
-  characters = data.characters;
-  getCharacters(0);
-});
-
-const getCharacters = (index) => {
-  if (index === characters.length) {
-    return;
-  }
-
-  request(characters[index], (error, response, body) => {
-    if (error) {
-      console.log(error);
-      return;
-    }
-    const characterData = JSON.parse(body);
-    console.log(characterData.name);
-    getCharacters(index + 1);
+function getStarWarsData (url) {
+  return new Promise((resolve, reject) => {
+    request.get(url, (err, response, body) => {
+      if (err) reject(err);
+      else resolve(JSON.parse(body));
+    });
   });
-};
+}
+
+async function fetchCharacterData (urls) {
+  if (urls.length === 0) {
+    return;
+  }
+
+  const characterUrl = urls.shift();
+  const characterData = await getStarWarsData(characterUrl);
+  console.log(characterData.name);
+
+  await fetchCharacterData(urls);
+}
+
+async function main () {
+  try {
+    const filmUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+    const starWarsFilmData = await getStarWarsData(filmUrl);
+    const characterUrls = starWarsFilmData.characters;
+
+    await fetchCharacterData(characterUrls);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
